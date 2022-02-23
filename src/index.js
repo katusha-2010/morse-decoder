89adf67f9851191964eff371152474424b6a6e50
katusha-2010 <katusha-2010@mail.ru>
2022-02-23 15:42:56 +0300

    :feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = "";
+let countArr = expr.length/10
+
+for(let i=0; i<countArr; i++){
+  let number = expr.slice(i*10, i*10 + 10 )
+  if(number === '**********') {
+    result += " ";
+  }
+  else {
+    let letter = "";
+    for(let k = 0; k < number.length/2; k++) {
+      let symbol = number.slice(k*2, k*2+2);
+      
+      if(symbol === "10") {
+        letter += ".";       
+      } else if(symbol === "11") {
+        letter += "-";
+      } else {
+        letter += "";
+      }     
+    }
+    result += MORSE_TABLE[letter] 
+  }  
+}
+return result    
 }
 
 module.exports = {
